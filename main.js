5d007892a889822c66a0b2e1eab9aa9fba4ecfdd
Михаил Гриб <gribmichael1998@gmail.com>
2021-09-15 15:38:36 +0300

    fix link errors

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const openmenu = document.getElementById('open_mobile')
 const exitmenu = document.getElementById('exit_mobile')
 const mobile = document.getElementById('mobilemenu')
-document.getElementById('open_mobile').addEventListener('click', () => {
+openmenu.addEventListener('click', () => {
 	mobile.style.display = 'flex'
 })
 exitmenu.addEventListener('click', () => {
 	mobile.style.display = 'none'
+})
+document.querySelectorAll('.mnav__link').forEach(el => {
+	el.addEventListener('click', () => mobile.style.display = 'none')
 })
